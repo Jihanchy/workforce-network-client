a03eb085e8cbebebaf7ab9026229aa1421f6569b
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-12-11 14:30:53 +0600

    added firebase etup

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,35 +1,60 @@
-import React from 'react';
+import Lottie from 'lottie-react';
+import React, { useState } from 'react';
+import animationRegisterLottie from '../../assets/register.json'
+import { FcGoogle } from "react-icons/fc";
 
 const Register = () => {
+    const [error,setError] = useState('')
+    const handleSignUp = e => {
+        e.preventDefault()
+        const form = e.target 
+        const name = form.name.value 
+        const email = form.email.value 
+        const password = form.password.value
+        const user = {name,email,password}
+        console.log(user)
+        if (!/^(?=.*[a-z])(?=.*[A-Z]).{6,}$/.test(password)) {
+            alert('Your password must have a lowerCase , an upperCase letter and at least 6 character')
+            return
+        }
+    }
     return (
         <div className="hero max-w-4xl mx-auto min-h-[470px]">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left flex-1">
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+            <div className="hero-content flex-col md:flex-row-reverse">
+                <div className="text-center lg:text-left w-96">
+                    <Lottie animationData={animationRegisterLottie}></Lottie>
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <h1 className="text-5xl font-bold m-8">Register now</h1>
-                    <form className="card-body">
+                <div className="card bg-base-100 text-center w-full max-w-sm shrink-0">
+                    <h1 className="text-4xl font-bold m-8">Register now</h1>
+                    <button className='btn hover:bg-white bg-white text-lg rounded-sm mb-3'>
+                        <span><FcGoogle /></span>
+                        <span className='hover:text-blue-500'>Sign up with Google</span>
+                    </button>
+                    <div className='divider'>or continue with</div>
+                    {/* form */}
+                    <form onSubmit={handleSignUp} className="card-body px-0 py-2">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="enter your name" className="input rounded-sm input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input rounded-sm input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="text" name='password' placeholder="password" className="input rounded-sm input-bordered" required />
                             <label className="label">
-
                             </label>
                         </div>
-                        <div className="form-control mt-6">
-                            <button className="btn bg-blue-500">Register</button>
+                        <div className="form-control">
+                            <button className="btn bg-blue-500 text-lg text-white">Register</button>
                         </div>
                     </form>
                 </div>
